211e1819ab78c0d317733c111b0908768eb3a20f
Jai Bhardwaj <0987sujals@gmail.com>
2025-07-13 15:41:27 +0530

    poafnds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Other Next.js configurations...
-  output: 'standalone', // Enables the standalone output mode
 };
 
 module.exports = nextConfig;
